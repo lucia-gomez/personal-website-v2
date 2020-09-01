4c5106c77e0db3ea472ff4206995c103f9119068
lucia-gomez <isabel.lucia.gomez@gmail.com>
2020-09-01 18:35:26 -0400

    css tweaks, link styling

--- src/components/section.js
+++ src/components/section.js
@@ -3,25 +3,15 @@ import PropTypes from 'prop-types'
 
 export default class Section extends React.Component {
   render() {
-    const leftSlant = (
-      <svg className='left-slant' viewBox="0 0 5 5" preserveAspectRatio="none">
-        <polygon points="5,5 0,0, 0,5" />
-      </svg>);
-
-    const rightSlant = (
-      <svg className='right-slant' viewBox="0 0 5 5" preserveAspectRatio="none">
-        <polygon points="0,5 5,0 5,5" />
-      </svg>
-    );
-
     const isEven = this.props.index % 2 === 0;
-
     return (
       <section id={this.props.id} className={isEven ? '-even-section' : '-odd-section'}>
         <div className='section-content'>
           {this.props.children}
         </div>
-        {isEven ? rightSlant : leftSlant}
+        <svg viewBox="0 0 5 5" preserveAspectRatio="none">
+          <polygon points={"0,5 5,5" + (isEven ? " 5,0" : " 0,0")} />
+        </svg>
       </section>
     );
   }
