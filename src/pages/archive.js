d7accd4113b7deef7741d5f244e79389e2a08c98
lucia-gomez <isabel.lucia.gomez@gmail.com>
2020-11-23 00:07:13 -0500

    create project archive page

---
+++ src/pages/archive.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from "../components/layout"
+import Section from "../components/section"
+import SectionTitle from "../components/sectionTitle"
+import { makePortfolioCard } from "./portfolio"
+import projects from "../scripts/projectList"
+
+export default function ArchivePage() {
+  return (
+    <Layout>
+      <Section id="archive" index={0}>
+        {SectionTitle("Things I've Made")}
+        <div className='portfolio-card-deck'>
+          {projects.map(makePortfolioCard)}
+        </div>
+      </Section>
+    </Layout>
+  );
+}
