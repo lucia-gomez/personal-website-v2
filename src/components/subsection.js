f53c8de808f0dc86839958ee9ecaf88d61a30c1b
lucia-gomez <isabel.lucia.gomez@gmail.com>
2021-05-17 17:54:18 -0400

    subsection styled component

--- src/components/subsection.js
+++ src/components/subsection.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components'
 
-export default class Subsection extends React.Component {
-  render() {
-    return (
-      <div className='subsection'>
-        <h4>{this.props.title}</h4>
-        {this.props.children}
-      </div>
-    )
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px 30px 5px 30px;
+  p {
+    text-align: left;
   }
+
+  span {
+    font-weight: 700;
+    background-color: ${props => props.theme.medium};
+    border-radius: 5px;
+    padding: 0px 3px;
+  }
+`;
+
+const Subsection = props => {
+  return (
+    <Wrapper>
+      <h4>{props.title}</h4>
+      {props.children}
+    </Wrapper>
+  )
 }
+
+export default Subsection;
