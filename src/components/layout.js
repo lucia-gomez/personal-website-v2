2e3c16398de4d7b561bbd8968a815889a20116fb
lucia-gomez <isabel.lucia.gomez@gmail.com>
2021-05-17 15:43:03 -0400

    update metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,6 +21,9 @@ const Layout = ({ children }) => {
         <Helmet>
           <meta charSet="utf-8" />
           <title>Lucia Gomez</title>
+          <meta name="description" content="Portfolio website for Lucia Gomez, full stack web developer" />
+          <meta name="keywords" content="Lucia, Gomez, web, developer, full, stack, full-stack, Javascript, React, Cornell, Facebook, ASL" />
+          <meta name="robots" content="index, follow" />
           <script src="https://kit.fontawesome.com/9dd13a1052.js" crossOrigin="anonymous"></script>
           <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-
         wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
