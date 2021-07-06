3997cd5d202a6fc20b631e69f70094bd42b61c13
lucia-gomez <isabel.lucia.gomez@gmail.com>
2021-07-05 20:53:55 -0400

    active tab scrolling indicator

--- src/components/tabbedContent.js
+++ src/components/tabbedContent.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Tab, Row, Col, Nav, Container } from 'react-bootstrap';
 
-const TabLink = styled(Nav.Link)`
-  color: ${props => props.theme.accent};
+const TabColumn = styled(Nav)`
+  position: relative;
+`;
+
+const TabActive = styled.div`
+  position: absolute;
+  top: 0px;
+  background-color: ${props => props.theme.accent};
+  width: 5px;
+  height: 40px;
+  z-index: 1;
+  transform: translateY(${props => props.position * 40}px);
+  transition: transform 300ms cubic-bezier(0.65, 0.05, 0.36, 1);
+`;
+
+const TabLink = styled(Nav.Link)` 
+  border-left: 5px solid ${props => props.theme.accentLight};
+  border-radius: 0px;
+  color: ${props => props.theme.text};
   text-align: left;
+  transition: background-color 300ms, color 300ms;
 
   :hover {
-    color: ${props => props.theme.accentHover};
+    background-color: ${props => props.theme.medium};
+    color: ${props => props.theme.header};
   }
 
   &.nav-link.active {
-    color: ${props => props.theme.header};
-    background-color: ${props => props.theme.accentLight};
-    border-left: 5px solid ${props => props.theme.accent};
+    color: ${props => props.theme.accent};
+    background-color: ${props => props.theme.medium};
   }
 `;
 
 const TabbedContent = props => {
+  const [position, setPosition] = useState(0);
+
   const tabs = Object.keys(props.tabs).map((tab, idx) =>
-    <Nav.Item key={idx}>
-      <TabLink eventKey={idx}>{tab}</TabLink>
-    </Nav.Item>
+    <TabLink eventKey={idx} key={idx}
+      onClick={() => setPosition(idx)}>{tab}</TabLink>
   );
 
   const contents = Object.values(props.tabs).map((content, idx) =>
@@ -35,11 +54,12 @@ const TabbedContent = props => {
       <Tab.Container defaultActiveKey={0}>
         <Row className="justify-content-center">
           <Col sm={3}>
-            <Nav variant="pills" className="flex-column">
+            <TabActive {...{ position }} />
+            <TabColumn className="flex-column">
               {tabs}
-            </Nav>
+            </TabColumn>
           </Col>
-          <Col sm={6}>
+          <Col sm={7}>
             <Tab.Content>
               {contents}
             </Tab.Content>
