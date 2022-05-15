8e196596d3e4ef40ed9bce1ba28ee34959c67a24
lucia-gomez <isabel.lucia.gomez@gmail.com>
2022-05-15 13:12:48 -0700

    swiper zine viewer

--- src/components/zines/zineFlipBook.js
+++ src/components/zines/zineFlipBook.js
@@ -1,96 +1,59 @@
-import { useRef } from 'react';
-import styled, { useTheme } from 'styled-components';
-import { Button } from '../button';
-import FlipPage from 'react-flip-page';
+import styled from 'styled-components';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Wrapper = styled.div`
   position: relative;
   width: fit-content;
   margin: auto;
-
-  @media only screen and (max-width: 576px) {
-    padding-top: 50px;
-  }
 `;
 
-const ButtonLeft = styled.div`
-  position: absolute;
-  transform: translateY(-50%);
-  top: 50%;
-  left: -50px;
-
-  @media only screen and (max-width: 576px) {
-    top: 0px;
-    left: 0px;
-    transform: none;
+const SwiperWrapper = styled.div`
+  .swiper {
+    max-width: 350px;
   }
-`;
 
-const ButtonRight = styled.div`
-  position: absolute;
-  transform: translateY(-50%);
-  top: 50%;
-  right: -50px;
-
-  @media only screen and (max-width: 576px) {
-    top: 0px;
-    right: 0px;
-    transform: none;
+  .swiper-wrapper {
+    padding-top: 30px;
   }
-`;
 
-const ArrowButton = styled(Button)`
-  margin: 0px;
+  .swiper-pagination-bullets {
+    bottom: unset;
+  }
+
+  .swiper-pagination-bullet {
+    background: ${props => props.theme.text};
+  }
+
+  .swiper-pagination-bullet-active {
+    background: ${props => props.theme.accent};
+    opacity: 1;
+  }
 `;
 
 export default function ZineFlipBook({ zine }) {
-  const theme = useTheme();
-  const flipBookRef = useRef(null);
-
-  const handlePreviousPage = () => {
-    if (flipBookRef.current != null) {
-      flipBookRef.current.gotoPreviousPage();
-    }
+  const getZineSwiper = zine => {
+    return (
+      <SwiperWrapper>
+        <Swiper pagination={true} modules={[Pagination]}>
+          {zine.pages.map((page, i) =>
+            <SwiperSlide key={i}>
+              <img src={page} style={{ height: '100%', width: '100%' }} alt='zine page' />
+            </SwiperSlide>
+          )}
+        </Swiper>
+      </SwiperWrapper>
+    );
   }
-
-  const handleNextPage = () => {
-    if (flipBookRef.current != null) {
-      flipBookRef.current.gotoNextPage();
-    }
-  }
-
-  const getZineFlipBook = (zine) => {
-    return <FlipPage
-      orientation="horizontal"
-      flipOnTouch={true}
-      showTouchHint={true}
-      height={zine.height}
-      animationDuration={500}
-      pageBackground={theme.bg}
-      ref={flipBookRef}>
-      {zine.pages.map((page, i) =>
-        <article key={i}>
-          <img src={page} style={{ height: '100%', width: '100%' }} alt='zine page' />
-        </article>
-      )}
-    </FlipPage>
-  };
 
   return (
     <Wrapper>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        {getZineFlipBook(zine)}
+        {getZineSwiper(zine)}
       </div>
-      <ButtonLeft>
-        <ArrowButton onClick={handlePreviousPage}>
-          <i className="fas fa-arrow-left"></i>
-        </ArrowButton>
-      </ButtonLeft>
-      <ButtonRight>
-        <ArrowButton onClick={handleNextPage}>
-          <i className="fas fa-arrow-right"></i>
-        </ArrowButton>
-      </ButtonRight>
     </Wrapper>
   );
 }
