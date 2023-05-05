a6bf2f0927c5c51e43f09af9c3834be5f4b862f9
lucia-gomez <isabel.lucia.gomez@gmail.com>
2023-05-04 23:02:20 -0700

    subscribe page clean up

--- src/components/email/subscribeButton.js
+++ src/components/email/subscribeButton.js
@@ -11,13 +11,6 @@ export default function SubscribeButton(props) {
         className="pas-button"
         onClick={() => {
           setShowModal(true)
-          // window.parent.postMessage(
-          //   {
-          //     eventId: "openWidgetModal",
-          //     widgetId: "0r72l",
-          //   },
-          //   "*"
-          // )
         }}
       >
         Subscribe
@@ -26,30 +19,6 @@ export default function SubscribeButton(props) {
         isShowing={showModal}
         handleClose={() => setShowModal(false)}
       />
-      <iframe
-        title="subscribe form"
-        data-w-token="b3083eb98d1d46f50cb4"
-        data-w-type="pop-in"
-        frameBorder="0"
-        scrolling="yes"
-        marginHeight="0"
-        marginWidth="0"
-        src="https://0r72l.mjt.lu/wgt/0r72l/zgu/form?c=eb57628d"
-        width="100%"
-        style={{ height: 0 }}
-      ></iframe>
-      {/* <iframe
-        title="subscribe trigger"
-        data-w-token="b3083eb98d1d46f50cb4"
-        data-w-type="trigger"
-        frameBorder="0"
-        scrolling="no"
-        marginHeight="0"
-        marginWidth="0"
-        src="https://0r72l.mjt.lu/wgt/0r72l/zgu/trigger?c=f3416f1d"
-        width="100%"
-        style={{ height: 0 }}
-      ></iframe> */}
     </>
   )
 }
--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -10,38 +10,10 @@ const Wrapper = styled.div`
   padding: 0 20px;
 `
 
-// const MailjetForm = styled.iframe`
-//   width: 100%;
-
-//   @media screen and (min-width: 840px) {
-//     width: 600px;
-//   }
-// `
-
-function setIFrameStyles() {
-  const iframe = document.getElementById("successMessageDiv"[0])
-  console.log(iframe)
-}
-
 export default function SubscribePage(props) {
-  setIFrameStyles()
   return (
     <Wrapper>
       <SubscribeForm />
-      {/* <SectionTitle>Sign up for my newsletter</SectionTitle>
-      <MailjetForm
-        title="subscribe form"
-        data-w-type="embedded"
-        frameBorder="0"
-        scrolling="no"
-        src="https://0r72l.mjt.lu/wgt/0r72l/z96/form?c=ae6857a7"
-        width="100%"
-        style={{ minHeight: 250 }}
-      ></MailjetForm>
-      <script
-        type="text/javascript"
-        src="https://app.mailjet.com/pas-nc-embedded-v1.js"
-      ></script> */}
     </Wrapper>
   )
 }
