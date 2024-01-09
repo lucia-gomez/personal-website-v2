4fa5dc14204f90f7219e7394b7ac0a9152c15ac8
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-01-09 15:14:11 -0500

    backup script

---
+++ server/db_backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Load the environment variables that have the super secret MongoDB password
+if [[ -f .env ]]; then
+    source .env
+fi
+
+ATLAS_USERNAME="admin"
+ATLAS_CLUSTER="luciagomez.xykba6v.mongodb.net"
+ATLAS_DATABASE="personalWebsite"
+
+# Construct the MongoDB Atlas URI
+MONGO_URI="mongodb+srv://${ATLAS_USERNAME}:${REACT_APP_MONGO_PASSWORD}@${ATLAS_CLUSTER}/${ATLAS_DATABASE}"
+
+BACKUP_DIR=backup
+
+# Create a timestamp for the backup folder
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+# Create the temporary backup directory
+mkdir -p $BACKUP_DIR/$TIMESTAMP
+
+# Run mongodump to create a backup in the temporary directory
+echo "Starting mongodump at $(date)"
+mongodump --uri $MONGO_URI --out $BACKUP_DIR/$TIMESTAMP
+echo "mongodump completed at $(date)"
+
+node awsUpload.js $BACKUP_DIR/$TIMESTAMP
